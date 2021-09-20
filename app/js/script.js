c8ed9e99a63f9bf8dd714ba33dbf92dd4fe21147
Feyisayo Ibukun <ibukun209@gmail.com>
2021-09-20 22:50:34 +0100

    Added scroll functionality and finished faq section for mobile

--- app/js/script.js
+++ app/js/script.js
@@ -1,10 +1,51 @@
 // for the toggles
 $(document).ready(
     () => {
-        $("#Q1").click(() => {
-            $(".accordian-answer").slideToggle("slow")
+        $("#C1").click(() => {
+            $("#Q1_ans").slideToggle("slow")
+            $("#Q1").toggleClass("weight")
+            
 
         })
     }
-    
+)
+
+$(document).ready(
+    () => {
+        $("#C2").click(() => {
+            $("#Q2_ans").slideToggle("slow")
+            $("#Q2").toggleClass("weight")
+
+        })
+    }
+)
+
+$(document).ready(
+    () => {
+        $("#C3").click(() => {
+            $("#Q3_ans").slideToggle("slow")
+            $("#Q3").toggleClass("weight")
+
+        })
+    }
+)
+
+$(document).ready(
+    () => {
+        $("#C4").click(() => {
+            $("#Q4_ans").slideToggle("slow")
+            $("#Q4").toggleClass("weight")
+
+        })
+    }
+)
+
+$(document).ready(
+    () => {
+        $("#C5").click(() => {
+            $("#Q5_ans").slideToggle("slow")
+            $("#Q5").toggleClass("weight")
+
+        })
+    }
 )
